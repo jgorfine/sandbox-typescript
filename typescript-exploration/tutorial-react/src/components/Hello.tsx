7fc7e8ff9311885cfe4a35e3c7d81742801a94d6
Jen Gorfine <jen.gorfine@trustify.co>
2017-04-26 17:37:12 -0400

    Made minor changes to the little Hello.tsx component.

--- typescript-exploration/tutorial-react/src/components/Hello.tsx
+++ typescript-exploration/tutorial-react/src/components/Hello.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface Props { 
+	compiler: string; 
+	framework: string; 
+}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
-export class Hello extends React.Component<HelloProps, undefined> {
+export class Hello extends React.Component<Props, undefined> {
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
